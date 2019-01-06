d376b6a8dbd70efdfec957bb05558f0d8a0cc19c
KAV <kav.develop@yandex.ru>
2019-01-06 10:18:05 +0700

    Сделал считывание из файла параметров

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# Программа запроса прогноза погоды с использованием API Yandex
+# Программа запроса текущей погоды с использованием API Yandex и методов GET/POST.
+# Данные выводятся с заднной периодичностью в консоль и пишутся в архивный .csv файл.
+
+import json
+
+class WeatherInformer:
+
+    def __init__(self, fileParams):
+        # Считываем необходимые для работы параметры из файла
+        with open(fileParams) as pfile:
+            self.params = json.load(pfile)
+        print(self.params)
+
+    def weather_info(self):
+
+        return True
+
+
+
+
+# ========= MAIN SECTION =============
+
+leninskoe = WeatherInformer('params.json')
