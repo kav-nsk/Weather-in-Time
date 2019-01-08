885d28c3ea4b7afb85b1769c9434c3dda5925b00
KAV <kav.develop@yandex.ru>
2019-01-08 07:47:19 +0700

    Сделал периодическое сохранение данных в .csv файл, открытие чистого файла при запуске программы, бесконечный цикл

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ from pprint import pprint
 class WeatherInformer:
 
     def __init__(self, fileParams):
-        # Считываем необходимые для работы параметры из файла
+        # Считываем необходимые для работы параметры из файла params.json
         with open(fileParams) as pfile:
             self.params = json.load(pfile)
-        self.questParams = {'lat': self.params['lat'], 'lon': self.params['lon'], 'hours': 'false', 'limit': '2'}
+
         self.nameArhFile = self.params['outputFile']
+        open(self.nameArhFile, 'w')
+        self.questParams = {'lat': self.params['lat'], 'lon': self.params['lon'], 'hours': 'false', 'limit': '2'}
+        self.periodCall = self.params['timer']
+        self.firstCall = True
 
     def weather_info(self):
         # Делаем запрос в Yandex
         url = 'https://api.weather.yandex.ru/v1/forecast'
         timeRequest = time.ctime()
-        response = requests.get(url, self.questParams, headers={'X-Yandex-API-Key': self.params['key']}).json()
+        req = requests.get(url, self.questParams, headers={'X-Yandex-API-Key': self.params['key']})
+        self.statusCode = req.status_code
+        response = req.json()
         response = [
                     {'param': 'время запроса', 'value': timeRequest[4:-5]},
                     {'param': 'время замера', 'value': time.ctime(response['fact']['obs_time'])[4:-5]},
@@ -32,22 +38,22 @@ class WeatherInformer:
                     {'param': 'скорость ветра', 'value': response['fact']['wind_speed'], 'unit': 'м/с'},
                     {'param': 'направление ветра', 'value': response['fact']['wind_dir']}
                     ]
-                    
+
         # Формирование шапки для csv
         columnName = [response[i]['param'] + ', ' + response[i]['unit'] for i in range(2, len(response) - 1)]
         columnName.insert(0, response[0]['param'])
         columnName.insert(1, response[1]['param'])
         columnName.append(response[-1]['param'])
-        print(columnName)
-        print()
 
         # Запись в архивный файл
-        with open(self.params['outputFile'], 'wt') as arhFile:
+        with open(self.nameArhFile, 'a') as arhFile:
             toArhFile = csv.writer(arhFile)
             writer = csv.DictWriter(arhFile, fieldnames=columnName)
-            writer.writeheader()
+            if self.firstCall:
+                writer.writeheader()
             writer.writerow({columnName[i]: response[i]['value'] for i in range(len(response))})
 
+        self.firstCall = False
         return
 
 
@@ -55,5 +61,8 @@ class WeatherInformer:
 
 # ========= MAIN SECTION =============
 
-leninskoe = WeatherInformer('params.json')
-leninskoe.weather_info()
+location = WeatherInformer('params.json')
+while True:
+    location.weather_info()
+    print('Код ответа:', location.statusCode)
+    time.sleep(location.periodCall)
